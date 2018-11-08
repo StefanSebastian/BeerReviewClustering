2dad248c13ade3efa23b1530275896cd93d717c4
StefanSebastian <stefansebii@gmail.com>
2018-11-08 21:45:20 +0200

    Added analysis

--- src/main/experiment/analysis.py
+++ src/main/experiment/analysis.py
@@ -4,6 +4,8 @@ from src.main.config import features_path, clusters_path, processed_path
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+import numpy as np
 
 
 def do_analysis(clusters_file, labels_file, words_file, data_file):
@@ -14,6 +16,7 @@ def do_analysis(clusters_file, labels_file, words_file, data_file):
 
     find_representative_words(clusters, words)
     make_heatmap(labels, beer_df)
+    evaluate_classification(labels, beer_df)
 
 
 def find_representative_words(clusters, words):
@@ -48,6 +51,56 @@ def make_heatmap(labels, beer_df):
     ax.set_ylabel('cluster_id', fontdict={'weight': 'bold', 'size': 26})
     plt.xticks(rotation=30)
     plt.show()
+
+
+def evaluate_classification(labels, beer_df):
+    beer_df['cluster_id'] = labels
+    # creates a df where each cluster id is mapped with its most common style
+    most_common_style_df = beer_df.groupby('cluster_id')['beer_style'].apply(lambda x: x.value_counts().index[0]).reset_index()
+
+    # adds a column 'cluster_syle' with the most common style in each discovered cluster
+    beer_df['cluster_style'] = beer_df['cluster_id'].map(most_common_style_df.set_index('cluster_id')['beer_style'])
+
+    predicted = beer_df['cluster_style'].tolist()
+    actual = beer_df['beer_style'].tolist()
+    cmat = confusion_matrix(actual, predicted)
+    print("Confusion matrix")
+    print(cmat)
+
+    # precision : for each row take the value of diag and divide by sum of col
+    precision = []
+    for col in range(len(cmat)):
+        up = cmat[col][col]
+        down = 0
+        for row in range(len(cmat)):
+            down += cmat[row][col]
+        precision.append(0 if down == 0 else up / down)
+    print('precision : ', precision)
+
+    # recall : value of diag divided by sum of values on row
+    recall = []
+    for row in range(len(cmat)):
+        up = cmat[row][row]
+        down = 0
+        for col in range(len(cmat)):
+            down += cmat[row][col]
+        recall.append(0 if down == 0 else up / down)
+    print('recall : ', recall)
+
+    mean_precision = 0
+    for p in precision:
+        mean_precision += p
+    mean_precision /= len(cmat)
+
+    mean_recall = 0
+    for r in recall:
+        mean_recall += r
+    mean_recall /= len(cmat)
+
+    print('Mean precision : ', mean_precision)
+    print('Mean recall : ', mean_recall)
+    print('F1 score : ', (2 * mean_precision * mean_recall) / (mean_precision + mean_recall))
+
 
 
 do_analysis('small.csv.features.centers.kd',
